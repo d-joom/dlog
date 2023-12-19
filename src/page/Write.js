5dee73869ae4b9f1047761eb532bd5056666c176
plana <jming6371@gmail.com>
2023-12-19 17:21:08 +0900

    write 페이지 퍼블리싱

--- src/page/Write.js
+++ src/page/Write.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "../css/main.css";
+import "../css/board.css";
 import {Editor} from '@toast-ui/react-editor';
 import '@toast-ui/editor/toastui-editor.css';
 
@@ -14,20 +14,24 @@ const Write = () => {
                         <option value="java">JAVA</option>
                         <option value="react">리액트</option>
                     </select>
-                    <input type="text" className="title_input"></input>
+                    <input type="text" className="title_input" placeholder='제목을 입력하세요.'></input>
                 </div>
                 
                 <div className="write_content_wrap">
-                <Editor
-                    height="400px"
-                    placeholder="Please Enter Text."
-                />
-
-
-                
+                    <Editor
+                        initialValue="hello react editor world!"
+                        placeholder='내용을 입력해주세요.'
+                        previewStyle="vertical"
+                        height="600px"
+                        initialEditType="wysiwyg"
+                        useCommandShortcut={false}
+                    />
+                </div>
+                <div class="btn_wrap">
+                    {/* <button className="outline_btn">임시저장</button> */}
+                    <button className="solid_btn">등록</button>
                 </div>
 
-                
             </div>
         </div>
     );
